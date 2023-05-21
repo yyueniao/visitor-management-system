e4a57f7f1c861d81cad26a354368cbb0b6c7396c
yyueniao <yyueniao0913@gmail.com>
2023-05-21 11:56:45 +0800

    style(php-cs-fixer): add php formatter

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,13 @@
+<?php
+
+$finder = PhpCsFixer\Finder::create()
+    ->exclude('vendor')
+    ->in(__DIR__);
+
+$config = new PhpCsFixer\Config();
+return $config->setRules([
+        '@PSR12' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'linebreak_after_opening_tag' => true,
+    ])
+    ->setFinder($finder);
